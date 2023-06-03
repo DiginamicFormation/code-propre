848f8fcd2e66174acc31660eeede610db4ca4fb5
FYHenry <frederic_yann.henry@protonmail.com>
2023-06-03 14:54:06 +0200

    Ex5 : done

--- src/main/java/ex5/Inventaire.java
+++ src/main/java/ex5/Inventaire.java
@@ -2,35 +2,48 @@ package ex5;
 
 import java.util.ArrayList;
 import java.util.List;
-
+/** Inventaire d’encaissement */
 public class Inventaire {
+    /** Ensemble de caisses exploitées */
+    private final List<Caisse> caisses;
 
-	private List<Caisse> caisses;
+    /**
+     * Construction de l’inventaire.
+     */
+    public Inventaire() {
+        this.caisses = new ArrayList<>(3);
+        final String[] noms = {"Petits", "Moyens", "Grands"};
+        final int[] extrema = {Integer.MIN_VALUE, 5, 20, Integer.MAX_VALUE};
+        for(int i=0; i<3; i++){
+            this.caisses.add(new Caisse(noms[i] + " objets",
+                    extrema[i],
+                    extrema[i+1]));
+        }
+    }
 
-	public Inventaire() {
-		caisses = new ArrayList<>();
-		caisses.add(new Caisse("Petits objets"));
-		caisses.add(new Caisse("Moyens objets"));
-		caisses.add(new Caisse("Grands objets"));
-	}
+    /**
+     * Ajouter un article.
+     * @param item Article
+     */
+    public void addItem(Item item) {
 
-	public void addItem(Item item) {
+        //TODO Faites évoluer ce code (idée: c'est la caisse qui doit
+        // "savoir" si elle peut accepter un objet ou non)
+        caisses.forEach(caisse -> caisse.add(item));
+    }
 
-		//TODO Faites évoluer ce code (idée: c'est le caisse qui doit "savoir" si elle peut accepter un objet ou non)
-		if (item.getPoids() < 5) {
-			caisses.get(0).getItems().add(item);
-		}
-		if (item.getPoids() >= 5 && item.getPoids() <= 20) {
-			caisses.get(1).getItems().add(item);
-		}
-		if (item.getPoids() >= 20) {
-			caisses.get(2).getItems().add(item);
-		}
-	}
+    /**
+     * Taille de l’inventaire en articles.
+     * @return Total d’articles
+     */
+    public int taille() {
 
-	public int taille() {
-		
-		//TODO faites évoluer ce code.
-		return caisses.get(0).getItems().size() + caisses.get(1).getItems().size() + caisses.get(2).getItems().size();
-	}
+        //TODO faites évoluer ce code.
+        int total = 0;
+        for(Caisse caisse: this.caisses){
+            total += caisse.total();
+        }
+        return total;
+
+    }
 }
